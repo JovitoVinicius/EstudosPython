d35c3fd45cde4275c64687ca955e0337961b93e3
JovitoVinicius <viniciusjovito@gmail.com>
2022-10-17 15:59:40 -0300

    Conhecendo a Linguaguem Python

---
+++ Conhecendo a Linguagem de Programação Python/convertendo.py
@@ -0,0 +1,9 @@
+print(int(10.89))
+
+print(int("100"))
+
+print(float("10.10"))
+
+print(float(100))
+
+print(type('Olá'))
---
+++ Conhecendo a Linguagem de Programação Python/variaveis_constantes.py
@@ -0,0 +1,10 @@
+nome =  "Jovito"
+idade = 22
+
+nome = "Vinicius"
+
+print(nome,idade)
+
+limite_saque = 1000
+
+STATES = ["DF","SP"]
