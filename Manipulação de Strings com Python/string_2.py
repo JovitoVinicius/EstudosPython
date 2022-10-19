5d894206d5b4fb6668aa4f00d2a9762d07c3761f
JovitoVinicius <viniciusjovito@gmail.com>
2022-10-19 13:38:34 -0300

    Manipulação de Strings 2

--- Manipulação de Strings com Python/string_2.py
+++ Manipulação de Strings com Python/string_2.py
@@ -15,5 +15,4 @@ print("Nome: {1} , Idade: {0}".format(idade, nome))
 #forma f-string
 
 print(f"Nome: {nome}, Idade: {idade}")
-
-print(f"Valor de PI: {PI:.2f}")
+print(f"Valor de PI: {PI:.2f}") 
--- Manipulação de Strings com Python/string_3.py
+++ Manipulação de Strings com Python/string_3.py
@@ -7,7 +7,7 @@ print(nome[:9])
 print(nome[9:])
 
 print(nome[10:16:2])
-
+#String Múltipla
 nome = "Vinicius"
 mensagem = f"""
 Olá meu nome é {nome},
